cad805ad79d8af78c2fcb964d23c7e11a0ac0b19
Nicklas Israelsson <nicklas.israelsson@episerver.com>
2020-11-17 14:14:05 +0100

    Dynamically add boxes
    Add a mouse tracking box that when clicked will deploy a box to the canvas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,26 @@
 import React, { useRef, useState } from 'react';
-import { Canvas, MeshProps, useFrame } from 'react-three-fiber';
+import { Canvas, MeshProps, useFrame, Vector3 } from 'react-three-fiber';
 import * as THREE from 'three';
 
 import './styles.scss';
 
 const Box = (props: MeshProps) => {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef<THREE.Mesh>();
-
+  const boxRef = useRef<THREE.Mesh>();
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => {
-    if (!mesh.current) return;
+    if (!boxRef.current) return;
 
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    boxRef.current.rotation.x = boxRef.current.rotation.y += 0.05;
   });
 
   return (
     <mesh
       {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      ref={boxRef}
       onClick={() => setActive(!active)}
       onPointerOver={() => setHover(true)}
       onPointerOut={() => setHover(false)}
@@ -34,13 +31,68 @@ const Box = (props: MeshProps) => {
   );
 };
 
+interface BoxPosition {
+  x: number;
+  y: number;
+}
+interface MouseBoxProps extends MeshProps {
+  onMouseBoxClicked: (boxPosition: BoxPosition) => void;
+}
+
+const MouseBox = ({ onMouseBoxClicked, ...rest }: MouseBoxProps) => {
+  const boxRef = useRef<THREE.Mesh>();
+
+  useFrame(({ mouse }) => {
+    if (!boxRef.current) return;
+    boxRef.current.position.x = mouse.x * 7.5;
+    boxRef.current.position.y = mouse.y * 3;
+  });
+
+  return (
+    <mesh
+      onClick={() => {
+        if (!boxRef.current) return;
+        onMouseBoxClicked({ x: boxRef.current.position.x, y: boxRef.current.position.y });
+      }}
+      {...rest}
+      ref={boxRef}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
+  );
+};
+
+const Boxes = ({ boxPositions }: { boxPositions: Vector3[] }) => {
+  return (
+    <>
+      {boxPositions.map((position, index) => (
+        <Box key={index} position={position} />
+      ))}
+    </>
+  );
+};
+
 export default () => {
+  const [boxPositions, setBoxPositions] = useState<Vector3[]>([]);
+
+  const addBox = (boxPosition: BoxPosition) => {
+    setBoxPositions([...boxPositions, [boxPosition.x, boxPosition.y, 0]]);
+  };
+
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.5, 0, 0]} />
-      <Box position={[2, 0, 0]} />
+
+      <MouseBox
+        onMouseBoxClicked={(boxPosition) => {
+          addBox(boxPosition);
+        }}
+        position={[0, 0, 0]}
+      />
+
+      <Boxes boxPositions={boxPositions} />
     </Canvas>
   );
 };
